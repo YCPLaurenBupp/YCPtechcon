e9b91e0a9d59be8ff15524c2fb4892c8e25eeb5e
YCPLaurenBupp <47125835+YCPLaurenBupp@users.noreply.github.com>
2019-04-17 19:22:31 -0400

    Menu and Schedule
    Configured the hamburger menu to have links and have the beginning of correct style and formatting. Updated all my additional pages to coincide with the new menu. Then started work on my schedule page by utilizing the tab feature https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_tabs. I am also thinking of using the accordion feature on another page https://www.w3schools.com/howto/howto_js_accordion.asp. I learned how to incorporate javascript including putting the menu script at the top of the page so the browser reads it correctly.

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginRight = "10px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
